20b2b7b903c39c0bc23c5b5124733ce6d8818d05
Carlos German Carreño Romano <charlieromano@gmail.com>
2021-06-27 21:15:48 -0300

    update json2csv.py to argv argc usage

--- Scripts/json2csv.py
+++ Scripts/json2csv.py
@@ -1,18 +1,46 @@
+import sys, getopt
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
 
-datasets = "../Datasets/"
-df = pd.read_json(datasets+"TECO2.2015.2021.json")
 
-df["ultimoPrecio"]
+#datasets = "../Datasets/"
+#df = pd.read_json(datasets+argv[1])
 
-dt=df[['fechaHora','ultimoPrecio',]]
-dt=dt.set_index(pd.DatetimeIndex(dt['fechaHora']))
+#df["ultimoPrecio"]
 
-dt=dt.sort_index(ascending=True)
-dt['ultimoPrecio'].plot()
-plt.show()
+#dt=df[['fechaHora','ultimoPrecio',]]
+#dt=dt.set_index(pd.DatetimeIndex(dt['fechaHora']))
+
+#dt=dt.sort_index(ascending=True)
+#dt['ultimoPrecio'].plot()
+#plt.show()
 
 
 
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print('json2csv.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('json2csv.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = str(argv[1])
+      elif opt in ("-o", "--ofile"):
+         outputfile = str(argv[3])
+   print('Input file is "', inputfile)
+   df = pd.read_json(inputfile)
+   dt=df[['fechaHora','ultimoPrecio',]]
+   dt=dt.set_index(pd.DatetimeIndex(dt['fechaHora']))
+   dt.to_csv(str(outputfile))
+   print('Output file is "', outputfile)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
