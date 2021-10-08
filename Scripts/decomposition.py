c5f403f6cbc2a5ec83a7dadbe467df440a7cad5a
Carlos German Carreño Romano <charlieromano@gmail.com>
2021-10-08 12:14:05 -0300

    agrego datasets para actividad 1

--- Scripts/decomposition.py
+++ Scripts/decomposition.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import statsmodels.tsa as tsa
 import statsmodels.api as sm
 
+period=90
 
 inputfile="../Datasets/RIGO.2012.2021.csv"
 print('Input file is "', inputfile)
@@ -22,7 +23,34 @@ plt.ylabel("AR$ ")
 plt.grid()
 plt.show()
 
-period=21
+
+# Example
 sm.tsa.seasonal_decompose(df, model='additive', freq=period).plot()
 plt.show()
 
+# Additive
+addit=sm.tsa.seasonal_decompose(df, model='additive', freq=period)
+addit.plot()
+
+# Multiplicative
+mult = sm.tsa.seasonal_decompose(df, model='multiplicative', freq=period)
+mult.plot(); plt.show()
+
+# Trend
+period=7
+addit=sm.tsa.seasonal_decompose(df, model='additive', freq=period)
+mult = sm.tsa.seasonal_decompose(df, model='multiplicative', freq=period)
+fig, axs = plt.subplots(3)
+fig.suptitle('Trends: additive vs. multiplicative')
+axs[0].plot(addit.trend)
+axs[1].plot(mult.trend)
+axs[2].plot(addit.trend)
+axs[2].plot(mult.trend)
+plt.show()
+
+period=90
+addit=sm.tsa.seasonal_decompose(df, model='additive', freq=period)
+mult = sm.tsa.seasonal_decompose(df, model='multiplicative', freq=period)
+plt.plot(addit.trend, color="blue")
+plt.plot(mult.trend, color="red")
+plt.show()
